872675200e7b6000b78ed33bf7012932266297cd
Isak96 <isak.kodehodet96@gmail.com>
2023-10-12 11:26:30 +0100

    Good start

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const hamburger = document.getElementById('hamburger');
+
+hamburger.addEventListener('click', () => {
+    // You can add your navigation menu or other actions here
+    alert('Hamburger clicked');
+});
