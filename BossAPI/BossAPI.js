36b671712a2fdd832cd03bb1965ae265f675edd5
Isak96 <isak.kodehodet96@gmail.com>
2023-10-16 14:46:11 +0100

    wtf, ingenting fungerer. brain is fried

--- BossAPI/BossAPI.js
+++ BossAPI/BossAPI.js
@@ -1,10 +1,54 @@
-const bossText = document.getElementById("boss-text");
-const apiButton = document.getElementById("apiButton");
+let limit = 200;
+let url = `https://eldenring.fanapis.com/api/bosses?limit=${limit}`;
 
-const fetchBoss = async () => {
-const response = await fetch("https://eldenring.fanapis.com/api/bosses?limit=100");
-const data = await response.json();
-bossText.textContent = data.data.value;
-};
+async function getData() {
+    try {
+        let response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data: ${response.statusText}`);
+        }
+        let json = await response.json();
+        console.log(json.data)
+        return json.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-apiButton.addEventListener("click", fetchBoss);
+const bossCardsContainer = document.getElementById("bossCards");
+
+async function createBossCards() {
+    const bossData = await getData();
+
+    if (!bossData) {
+        // Handle the case where data retrieval fails.
+        return;
+    }
+
+    bossData.map((boss) => {
+        const bossCard = document.createElement("div");
+        bossCard.classList.add("boss-card"); // You can define CSS styles for boss cards.
+
+        // Create the card content using the boss data.
+        bossCard.innerHTML = `
+            <h2>${boss.name}</h2>
+            <p><strong>Region:</strong> ${boss.region}</p>
+            <p><strong>Description:</strong> ${boss.description}</p>
+            <p><strong>Location:</strong> ${boss.location}</p>
+            <p><strong>Drops:</strong></p>
+            <ul>
+                ${boss.drops.map((drop) => `<li>${drop}</li>`).join("")}
+            </ul>
+            <p><strong>Health Points:</strong> ${boss.healthPoints}</p>
+        `;
+
+        // Append the card to the container.
+        bossCardsContainer.appendChild(bossCard);
+    });
+
+    // Log a message indicating that boss data retrieval and card creation are complete.
+    console.log("Boss data retrieval and card creation are complete.");
+}
+
+// Call the function to create boss cards.
+createBossCards();
